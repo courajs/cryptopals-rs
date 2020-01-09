6dad55acdfa908c9d8e48f9d3e0e6e2014378972
Aaron Sikes <aaron@sikes.io>
2020-01-09 18:01:33 -0500

    Solve (ish) challenge #3
    Might need a better scorer?

---
+++ src/set1/c3.rs
@@ -0,0 +1,151 @@
+use std::collections::HashMap;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    // taken from https://norvig.com/mayzner.html
+    static ref FREQS: HashMap<char, f64> = [
+        ('E', 0.1249),
+        ('T', 0.0928),
+        ('A', 0.0804),
+        ('O', 0.0764),
+        ('I', 0.0757),
+        ('N', 0.0723),
+        ('S', 0.0651),
+        ('R', 0.0628),
+        ('H', 0.0505),
+        ('L', 0.0407),
+        ('D', 0.0382),
+        ('C', 0.0334),
+        ('U', 0.0273),
+        ('M', 0.0251),
+        ('F', 0.0240),
+        ('P', 0.0214),
+        ('G', 0.0187),
+        ('W', 0.0168),
+        ('Y', 0.0166),
+        ('B', 0.0148),
+        ('V', 0.0105),
+        ('K', 0.0054),
+        ('X', 0.0023),
+        ('J', 0.0016),
+        ('Q', 0.0012),
+        ('Z', 0.0009),
+    ].iter().cloned().collect();
+}
+
+fn calc_letter_freqs(s: &str) -> Option<HashMap<char, f64>> {
+    let mut counts: HashMap<char, usize> = HashMap::with_capacity(26);
+    for c in b'A'..=b'Z' {
+        counts.insert(c as char, 0);
+    }
+    let mut total = 0;
+    for c in s.to_ascii_uppercase().chars() {
+        if c.is_ascii_uppercase() {
+            total += 1;
+            let n = counts.get_mut(&c).unwrap();
+            *n += 1;
+        }
+    }
+
+    if total > 0 {
+        Some(counts.into_iter().map(|(k,v)| (k, v as f64 / total as f64)).collect())
+    } else {
+        None
+    }
+}
+
+fn mean_square(i: impl Iterator<Item = f64>) -> Option<f64> {
+    let mut sum = 0.0;
+    let mut count = 0;
+    for n in i {
+        count += 1;
+        sum += n*n;
+    }
+    if count > 0 {
+        Some(sum / (count as f64))
+    } else {
+        None
+    }
+}
+
+fn score_text(s: &str) -> Option<f64> {
+    calc_letter_freqs(s).and_then(|freqs| {
+        mean_square(FREQS.iter().map(|(c, freq)| {
+            let n = freqs.get(c).unwrap() - freq;
+            n*n
+        }))
+    })
+}
+
+fn byte_xor(s: &[u8], b: u8) -> Vec<u8> {
+    s.iter().map(|c| c ^ b).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn c3() {
+        let input = hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
+
+        let mut results = Vec::new();
+
+        for c in std::u8::MIN..=std::u8::MAX {
+            let rotated = byte_xor(&input, c);
+            if let Ok(s) = String::from_utf8(rotated) {
+                if let Some(score) = score_text(&s) {
+                    dbg!(c, &s);
+                    results.push((score, s));
+                }
+            }
+        }
+
+        // use std::cmp::partial_cmp;
+        results.sort_by(|a,b| {
+            // println!("{}, {}", a.0, b.0);
+            a.0.partial_cmp(&b.0).unwrap()
+        });
+
+        for i in 0..10 {
+            println!("{}: {}", results[i].0, results[i].1);
+        }
+
+        todo!()
+    }
+
+    #[test]
+    fn test_tally() {
+        let input = "aaaabbcd";
+        let expected: HashMap<char, f64> = [
+            ('A', 0.5),
+            ('B', 0.25),
+            ('C', 0.125),
+            ('D', 0.125),
+            ('E', 0.0), ('T', 0.0), ('O', 0.0),
+            ('I', 0.0), ('N', 0.0), ('S', 0.0),
+            ('R', 0.0), ('H', 0.0), ('L', 0.0),
+            ('U', 0.0), ('M', 0.0), ('F', 0.0),
+            ('P', 0.0), ('G', 0.0), ('W', 0.0),
+            ('Y', 0.0), ('V', 0.0), ('K', 0.0),
+            ('X', 0.0), ('J', 0.0), ('Q', 0.0),
+            ('Z', 0.0),
+        ].iter().cloned().collect();
+
+        assert_eq!(expected, calc_letter_freqs(&input).unwrap());
+    }
+
+    #[test]
+    fn test_mean_square() {
+        let input = vec![1.0, 2.0, 3.0];
+        assert_eq!(mean_square(input.into_iter()).unwrap(), 14.0f64 / 3.0);
+    }
+
+    #[test]
+    fn test_single_byte_xor() {
+        let input = vec![0b11111111, 0b00000000, 0b10101010];
+        let b = 0b11100111;
+        assert_eq!(byte_xor(&input, b), vec![0b00011000, 0b11100111, 0b01001101]);
+    }
+}
